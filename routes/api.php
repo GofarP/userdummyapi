b4740185c3cd828c98b184f6250e48f1ad9a9ff9
Gofar Putra <gofarperdana83@gmail.com>
2025-08-08 06:12:10 +0700

    Fix route

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('users', UserController::class);
 Route::get('/user', [UserController::class, 'dataWithPagination']);
 
-Route::get('/usersearch', [UserController::class, 'searchUser']);
+Route::get('/searchuser', [UserController::class, 'searchUser']);
