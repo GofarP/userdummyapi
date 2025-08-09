3f635071793c315fd1d9ff8a17a760fd97a4df13
Gofar Putra <gofarperdana83@gmail.com>
2025-08-09 14:09:04 +0700

    remove unused code

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [UserController::class, 'dataWithPagination']);
 
