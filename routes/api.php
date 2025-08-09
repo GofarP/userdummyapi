97e57aa782d584c8641c40f954c4aa214b1c2eef
Gofar Putra <gofarperdana83@gmail.com>
2025-08-09 16:22:55 +0700

    remove auth:api

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Api\UserController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [UserController::class, 'dataWithPagination']);
 
-Route::middleware(['jwt.auth','jwt.cookie', 'auth:api'])->group(function () {
+Route::middleware(['jwt.auth','jwt.cookie'])->group(function () {
     Route::apiResource('users', UserController::class);
     Route::get('/searchuser', [UserController::class, 'searchUser']);
 });
